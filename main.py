1fff738cf8d12aa3ae8f1751b7024a05a97cedc4
Deniz Gökçin <dgokcin@gmail.com>
2020-05-24 10:48:34 +0300

    progress

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import cv2
 import pylab as plt
@@ -33,9 +35,55 @@ def draw_keypoints(img, kp):
     plt.imshow(keypoints)
     plt.show()
 
+def custom_listdir(directory):
+    """A specialized version of os.listdir() that ignores files that
+    start with a leading period."""
+    filelist = os.listdir(directory)
+    return [x for x in filelist
+            if not (x.startswith('.'))]
+
+def train_model(path):
+    images = custom_listdir(path)
+    return images
+
+def get_images(path):
+    imgs = []
+    for root, d_names, f_names in os.walk(path):
+        for f in f_names:
+            imgs.append(os.path.join(root, f))
+
+    return imgs
+
+
 if __name__ == '__main__':
     img = cv2.imread('dataset/airplanes_train/img005.jpg')
 
-    des_grid = get_descriptors_grid(img, step_size=10)
+    train_path = 'dataset-modified/train'
+    test_path = 'dataset-modified/test'
+
+    # Get the training classes names and store them in a list
+    images = get_images(train_path)
+    train_labels = np.array([])
+
+    for path in images:
+        if ("airplanes" in path):
+            class_index = 0
+        elif ("cars" in path):
+            class_index = 1
+        elif ("motorbikes" in path):
+            class_index = 2
+        elif ("faces" in path):
+            class_index = 3
+
+        train_labels = np.append(train_labels, class_index)
+
+    a=1
+
+
+
+
+
+
+    # des_grid = get_descriptors_grid(img, step_size=10)
     des_key = get_descriptors_default(img)
 
